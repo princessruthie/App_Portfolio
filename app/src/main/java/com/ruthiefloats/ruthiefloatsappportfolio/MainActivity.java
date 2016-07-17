749a117c33c60dce377a453abcfd06c9df71c448
princessruthie <missruthie@gmail.com>
2016-07-17 18:54:35 -0400

    Initial commit

---
+++ app/src/main/java/com/ruthiefloats/ruthiefloatsappportfolio/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ruthiefloats.ruthiefloatsappportfolio;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
